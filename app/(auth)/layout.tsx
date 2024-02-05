fecdc75dda6162b9e29127db65303b4ed7413e8a
TenzDelek <tibetdelek@gmail.com>
2024-02-05 22:19:09 +0530

    integrated clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+//this kayout is specificlly for the pages in (auth)
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import '../globals.css'
+export const metadata={
+    title:'CAPSTONE VITGRAM',
+    description:'A Next.js 14 VITgram App'
+}
+
+const inter=Inter({subsets:['latin']})
+
+export default function RootLayout({
+    children
+}:{
+    children:React.ReactNode
+}){
+    return (
+    <ClerkProvider>
+        <html lang="en">
+            <body className={`${inter.className} bg-dark-1`}>
+                {children}
+            </body>
+        </html>
+    </ClerkProvider>
+    )
+}
