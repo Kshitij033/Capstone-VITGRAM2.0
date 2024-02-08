6edc35d26bfcfaa6c6eb7e1c16960838aa04b691
TenzDelek <tibetdelek@gmail.com>
2024-02-08 10:57:40 +0530

    shadcn

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,31 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs"
  async function Page(){
+    const user=await currentUser();
+    const userInfo={}
+    const userData={
+        // id: user?.id,
+        // objectId:userInfo?._id,
+        // username=userInfo?.username || user?.username,
+        // name:userInfo?.name || user?.firstName || "",
+        // bio: userInfo?.bio || "",
+        // image:userInfo?.image || user?.imageUrl
+        id: user?.id,
+        objectId:"userInfo?._id",
+        username: "userInfo?.username || user?.username",
+        name:"userInfo?.name || user?.firstName || ",
+        bio: "userInfo?.bio" || "",
+        image: user?.imageUrl
+    }
     return (
-        <main>
+        <main className=" py-20 px-10 justify-start mx-auto flex max-w-3xl flex-col">
             <h1 className=" head-text">Onboarding</h1>
+            <p className=" mt-3  text-base-regular text-light-2">
+                Complete your profile now to use VITgram
+            </p>
+            <section className=" mt-9 bg-dark-2 p-10">
+            <AccountProfile user={userData} btnTitle="Continue"/>
+            </section>
         </main>
     )
  }
