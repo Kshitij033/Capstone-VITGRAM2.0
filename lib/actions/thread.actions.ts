495329072716c6dfae79b21bd33b1175e43261fb
TenzDelek <tibetdelek@gmail.com>
2024-03-17 10:48:53 +0530

    updated actions for fetching individual post and created a dynamic page for the post

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -64,3 +64,42 @@ export async function fetchPosts(pageNumber=1,pageSize=20){
     const isNext=totalPostsCount > skipAmount+posts.length;
     return {posts,isNext}
 }
+
+export async function fetchThreadById(id:string) {
+    connectToDB()
+    try {
+        const thread=await Thread.findById(id)
+        .populate(
+            {
+                path:'author',
+                model:User,
+                select:'_id id name image'
+            }
+        )
+        .populate({
+            path: "children", // Populate the children field
+            populate: [
+              {
+                path: "author", // Populate the author field within children
+                model: User,
+                select: "_id id name parentId image", // Select only _id and username fields of the author
+              },
+              {
+                path: "children", // Populate the children field within children
+                model: Thread, // The model of the nested children (assuming it's the same "Thread" model)
+                populate: {
+                  path: "author", // Populate the author field within nested children
+                  model: User,
+                  select: "_id id name parentId image", // Select only _id and username fields of the author
+                },
+              },
+            ],
+          })
+          .exec();
+    
+        return thread;
+    } catch (error) {
+        console.error("Error while fetching thread:", error);
+        throw new Error("Unable to fetch thread");
+    }
+}
