5d2cbc17b7f4146a2beb57c550ac5101fb32b315
TenzDelek <tibetdelek@gmail.com>
2024-03-18 09:59:00 +0530

    finished the comment section

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -29,11 +29,27 @@ const Page =async ({ params }: { params: { id: string } }) => {
     </div>
     <div className=" mt-7">
       <Comment 
-      threadId={thread.id}
+      threadId={params.id}
       currentUserImg={user.imageUrl} //from clerk
       currentUserId={JSON.stringify(userInfo._id)} //userinfo is from database
       />
     </div>
+    <div className='mt-10'>
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            key={childItem._id}
+            id={childItem._id}
+            currentUserId={user.id}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
+      </div>
   </section>
 )
 }
