90a3f4edb3246e235ed8274608c7cd3c2540731b
TenzDelek <tibetdelek@gmail.com>
2024-03-20 11:15:39 +0530

    added action for activity and fixed the auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
     <ClerkProvider>
         <html lang="en">
             <body className={`${inter.className} bg-dark-1`}>
+                <div className=" min-h-screen w-full flex justify-center items-center">
                 {children}
+                </div>
             </body>
         </html>
     </ClerkProvider>
