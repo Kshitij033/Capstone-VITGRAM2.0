a508b94727dd2c5b01707508d4dccc1fa1772f46
TenzDelek <tibetdelek@gmail.com>
2024-04-08 21:37:17 +0530

    webhook ,actions and model created for community

---
+++ lib/models/community.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const communitySchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  createBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const Community = mongoose.models.Community || mongoose.model("Community", communitySchema);
+//the first time the mongose.models.user doesnt exist so it create using the second option
+//but after that it using the instance of the first
+export default Community;
