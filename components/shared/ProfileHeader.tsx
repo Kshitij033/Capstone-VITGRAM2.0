ffa17339c155981329b6b4453d7b287d77774122
TenzDelek <tibetdelek@gmail.com>
2024-05-08 12:21:29 +0530

    bio

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -43,7 +43,7 @@ function ProfileHeader({
        
       </div>
 
-      <p className='mt-6 max-w-lg text-base-regular text-light-2'>VIT Vellore</p>
+      <p className='mt-6 max-w-lg text-base-regular text-light-2'>{bio}</p>
 
       <div className='mt-12 h-0.5 w-full bg-dark-3' />
     </div>
